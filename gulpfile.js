1379b60810ef1767405ff1faf765b7605ca9ad47
Ekaterina1994Zakharova <Loedoriaa@gmail.com>
2021-11-04 21:50:21 +0200

    конвертизация шрифта otf в ttf

--- gulpfile.js
+++ gulpfile.js
@@ -37,7 +37,24 @@ const scss = require('gulp-sass')(require('sass')),
     rename = require("gulp-rename"),
     uglify = require("gulp-uglify-es").default,
     imagemin = require("gulp-imagemin"),
-    webp = require("gulp-webp");
+    webp = require("gulp-webp"),
+    webphtml = require("gulp-webp-html"),
+    webpcss = require("gulp-webpcss"),
+    svgSprite = require("gulp-svg-sprite"),
+    ttf2woff = require("gulp-ttf2woff"),
+    ttf2woff2 = require("gulp-ttf2woff2"),
+    fonter = require("gulp-fonter");
+
+
+
+function fonts(params) {
+   src(path.src.fonts)
+       .pipe(ttf2woff())
+       .pipe(dest(path.build.fonts));
+   return src(path.src.fonts)
+       .pipe(ttf2woff2())
+       .pipe(dest(path.build.fonts));
+};
 
 
 function browserSync(params) {
@@ -53,6 +70,7 @@ function browserSync(params) {
 function html() {
     return src(path.src.html)
         .pipe(fileinclude())
+        .pipe(webphtml())
         .pipe(dest(path.build.html))
         .pipe(browsersync.stream())
 }
@@ -74,6 +92,7 @@ function css() {
                 cascade: true
             })
         )
+        .pipe(webpcss())
         .pipe(dest(path.build.css))
         .pipe(clean_css())
         .pipe(
@@ -118,6 +137,32 @@ function images() {
         .pipe(browsersync.stream())
 }
 
+gulp.task("otf2ttf", function () {
+    return src([source_folder + "/fonts/*.otf"])
+        .pipe(fonter({
+            formats: ["ttf"]
+        }))
+        .pipe(dest(source_folder + "/fonts/"));
+})
+
+
+
+gulp.task( "svgSprite", function () {
+    return gulp.src([source_folder + "/iconsprite/*.svg"])
+        .pipe(svgSprite({
+            mode: {
+                stack: {
+                    sprite: "../icons/icons.svg",  //sprite file name
+                    example: true
+                }
+            },
+            })
+
+        )
+        .pipe(dest(path.build.img))
+})
+
+
 function watchFiles(params) {
     gulp.watch([path.watch.html], html);
     gulp.watch([path.watch.css], css);
@@ -129,9 +174,10 @@ function clean(params) {
     return del(path.clean);
 }
 
-let build = gulp.series(clean, gulp.parallel(css, js, html, images));
+let build = gulp.series(clean, gulp.parallel(css, js, html, images, fonts));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.fonts = fonts;
 exports.images = images;
 exports.js = js;
 exports.css = css;
